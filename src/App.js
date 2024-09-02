c8f56431535f5aac6e1eb9251be8e47c10b59a4b
Arvin <arvincris@gmail.com>
2024-09-02 16:42:11 +0800

    Deleting an Item, using flow inversion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Chargers", quantity: 3, packed: true },
-  { id: 4, description: "Powebank", quantity: 1, packed: true },
-];
-
 function App() {
   const [items, setItems] = useState([]);
 
@@ -14,11 +7,16 @@ function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    alert(id);
+    setItems(items => items.filter(item => item.id !== id));
+  }
+
   return (
     <div className="app">
       <Logo/>
       <Form onAddItems={handleAddItems}/>
-      <PackingList items={items}/>
+      <PackingList items={items} onDeleteItem={handleDeleteItem}/>
       {/* {initialItems.map(item => <PackingList key={item.id} item={item}/>) } */}
       <Stats/>
     </div>
@@ -63,11 +61,11 @@ function Form({onAddItems}) {
   )
 }
 
-function PackingList({items}) {
+function PackingList({items, onDeleteItem}) {
   return (
     <div className="list">
       <ul>
-        {items.map(item => <Item key={item.id} item={item}/>)}
+        {items.map(item => <Item key={item.id} item={item} onDeleteItem={onDeleteItem}/>)}
       </ul>
     </div>
   );
@@ -80,11 +78,11 @@ function Stats() {
   )
 }
 
-function Item({item}) {
+function Item({item, onDeleteItem}) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>{item.quantity} {item.description}</span>
-      <button>❌</button>
+      <button onClick={(e) => onDeleteItem(item.id)}>❌</button>
     </li>
   )
 }
