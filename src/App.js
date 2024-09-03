19630ae3bd758ccaf14797a574673eff286dced6
Arvin <arvincris@gmail.com>
2024-09-03 12:07:50 +0800

    Sorting the Items

--- src/App.js
+++ src/App.js
@@ -65,11 +65,36 @@ function Form({onAddItems}) {
 }
 
 function PackingList({items, onDeleteItem, onToggleItem}) {
+  const [sortBy, setSortBy] = useState('input');
+
+  let sortedItems;
+
+  // Sorting by the input order
+  if (sortBy === 'input') sortedItems = items;
+
+  // Sorting based in the item description
+  if (sortBy === 'description') sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+  // if (sortBy === 'description') sortedItems = [...items].sort((a, b) => a.description.localeCompare(b.description));
+  // if (sortBy === 'description') sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+
+  // Sorting based on the packed status
+  if (sortBy === 'packed') sortedItems = [...items].sort((a, b) => Number(a.packed) - Number(b.packed));
+
+
+  console.log(sortedItems);
+
   return (
     <div className="list">
       <ul>
-        {items.map(item => <Item key={item.id} item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}/>)}
+        {sortedItems.map(item => <Item key={item.id} item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}/>)}
       </ul>
+      <div className="actions">
+        <select onChange={(e) => setSortBy(e.target.value)} value={sortBy}>
+          <option value="input">Sort by the input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
